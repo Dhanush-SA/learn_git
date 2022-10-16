2c15db43dcfe7a88802f746ef5b5834a29a2ff3f
= <=>
2022-10-16 18:16:03 +0530

    first mrg conf

--- learn_git.cpp
+++ learn_git.cpp
@@ -4,11 +4,6 @@ using namespace std;
 int main(){
     cout << "Hello, Welcome to the learning world!";
     cout << "second_commit";
-<<<<<<< HEAD
-  
-    int a = 2;
-=======
     cout <<"third commit";
     int a = 5;
->>>>>>> 8ea32ad0b82fa0cfedb8d2ad8bcb3bfa851d0369
 }
